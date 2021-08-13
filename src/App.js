25c771962742f5f96be49909f96b572d4b1abe46
Norman Jeune III <normanjeuneiii@Normans-MacBook-Pro-2.local>
2021-08-13 10:59:03 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import handlers from './mocks/handlers';
+import Characters from './components/Character';
+
 
 const App = () => {
+
+  const [SwData, setSwData] = useState({})
+  const [error, setError] = useState(null)
+
+useEffect(() => {
+  axios.get(`${BASE_URL}/apod?api_key=${API_KEY}`)//external server request
+    .then(res => {
+      console.log(res.data)
+      setSwData(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+      setError("Sorry, the force is not with you!");
+    })
+}, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +29,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Major Characters List</h1>
+      <Characters name={SwData.name} height={SwData.height} mass={SwData.mass} hair_color={SwData.hair_color} skin_color={SwData.skin_color} eye_color={SwData.eye.eye_color} birth_year={SwData.birth_year} gender={SwData.gender} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacters = styled.div`
+    color: ${(pr) => pr.theme.secondaryColor};
+    `
+
+function Characters(props) {
+    return (
+        <StyledCharacters>
+          <p>{props.characters}</p>
+        </StyledCharacters>
+    )
+
+}
+
+export default Characters
